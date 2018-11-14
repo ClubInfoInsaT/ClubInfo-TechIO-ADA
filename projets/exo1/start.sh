5a4d531056006885f4c270265a5ad20325f1f94a
Walleza1 <24304667+Walleza1@users.noreply.github.com>
2018-11-14 17:44:03 +0100

    Testing user output ok

--- projets/exo1/start.sh
+++ projets/exo1/start.sh
@@ -5,6 +5,7 @@ gnatmake hello_world_correction.adb
 
 res=`./hello_world`
 correct=`./hello_world_correction`
+rm *.o *.ali hello_world hello_world_correction
 if [ "$res" = "$correct" ]
 then
 	echo "TECHIO> success true"
