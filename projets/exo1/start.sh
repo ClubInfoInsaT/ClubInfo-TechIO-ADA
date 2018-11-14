dbd6f39ff8b9d1a67820045470f562f037649187
Walleza1 <24304667+Walleza1@users.noreply.github.com>
2018-11-14 17:38:09 +0100

    Start sh is ok

--- projets/exo1/start.sh
+++ projets/exo1/start.sh
@@ -3,10 +3,11 @@
 gnatmake hello_world.adb
 gnatmake hello_world_correction.adb
 
-res=./hello_world
-correct=./hello_world_correction
-if ($res==$correct)
-then	echo "Ok"
+res=`./hello_world`
+correct=`./hello_world_correction`
+if [ "$res" = "$correct" ]
+then
+	return 0
 else
-	echo "Nok"
-
+	echo 1
+fi
