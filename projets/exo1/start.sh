659773dbf3341b6b0ab42ac467d1bb5530d15e28
Walleza1 <24304667+Walleza1@users.noreply.github.com>
2018-11-14 20:10:08 +0100

    Testing again

--- projets/exo1/start.sh
+++ projets/exo1/start.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-gnatmake hello_world.adb
-gnatmake hello_world_correction.adb
+gnatmake -q hello_world.adb
+gnatmake -q hello_world_correction.adb
 
 res=`./hello_world`
 correct=`./hello_world_correction`
