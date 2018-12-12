3e1d17d746ff7600f4369f2d52bf4c62f2bc37d7
Walleza1 <24304667+Walleza1@users.noreply.github.com>
2018-12-12 13:19:42 +0100

    Gada try

--- projets/exo1/hello_world.adb
+++ projets/exo1/hello_world.adb
@@ -1,5 +1,20 @@
-with Text_IO;
-procedure Hello_World is
+with GAda.Text_IO;
+procedure Hello is
 	begin
-		-- Ecrivez ici
-end Hello_World;
+		GAda.Text_IO.Put("Hello");
+    GAda.Text_IO.New_Line;
+    GAda.Text_IO.Put_Line("Tapez une chaine ");
+    GAda.Text_IO.Put_Line(GAda.Text_IO.Normalise("AAAA",3)&" OK");
+    GAda.Text_IO.Put_Line(GAda.Text_IO.Normalise("BB",3)& " NoK");
+    GAda.Text_IO.Put_Line(GAda.Text_IO.Normalise("C",3)& "Nook");
+    GAda.Text_IO.Put('C');
+    GAda.Text_IO.Put_Line('D');
+    GAda.Text_IO.Put("Tapez une chaine ");
+    declare
+      text: String :=GAda.Text_IO.FGet; 
+      begin
+      GAda.Text_IO.Put_Line("New Value "&text);
+    end;
+		GAda.Text_IO.Put("Test replace");
+		GAda.Text_IO.Replace("Nope");
+end Hello;
--- projets/exo1/start.sh
+++ projets/exo1/start.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
 gnatmake -q hello_world.adb
-sleep(10)
 echo "TECHIO> terminal -i ./hello_world"
