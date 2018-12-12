8ab79bea86c8e03f53dab737a3510309515a2fb5
Walleza1 <24304667+Walleza1@users.noreply.github.com>
2018-12-12 13:23:29 +0100

    Correcting name file error

--- projets/exo1/hello_world.adb
+++ projets/exo1/hello_world.adb
@@ -1,7 +1,7 @@
 with GAda.Text_IO;
-procedure Hello is
+procedure Hello_world is
 	begin
-		GAda.Text_IO.Put("Hello");
+		GAda.Text_IO.Put("Hello_world");
     GAda.Text_IO.New_Line;
     GAda.Text_IO.Put_Line("Tapez une chaine ");
     GAda.Text_IO.Put_Line(GAda.Text_IO.Normalise("AAAA",3)&" OK");
@@ -17,4 +17,4 @@ procedure Hello is
     end;
 		GAda.Text_IO.Put("Test replace");
 		GAda.Text_IO.Replace("Nope");
-end Hello;
+end Hello_world;
