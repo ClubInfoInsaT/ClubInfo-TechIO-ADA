99d26df1c55be2efc096346b5b92fe40df468ec3
Walleza1 <24304667+Walleza1@users.noreply.github.com>
2019-01-23 09:29:14 +0100

    Quickfix

--- projets/Bonus/start.sh
+++ projets/Bonus/start.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
 gnatmake -aI/Sources $1 -o insa-ada-runner
 echo "TECHIO> terminal -i ./insa-ada-runner"
+
 running=0
 terminated=0
+
 while [ $terminated -ne 1 ];do
 	res=`ps -ef |grep "insa-ada-runner"|wc -l`
 	if [ $res -gt 1 ]
 		then running=1
 	else
-		if [ $running -eq 1 ]
+		if [ $running -eq 1 ]
 			then terminated=1
 		fi	
 	fi
