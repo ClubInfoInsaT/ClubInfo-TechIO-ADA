efb2c9d59d3848fb10f005a107cb53938e067432
Walleza1 <24304667+Walleza1@users.noreply.github.com>
2019-01-23 09:08:32 +0100

    Just sleep 10

--- projets/Bonus/start.sh
+++ projets/Bonus/start.sh
@@ -1,24 +1,5 @@
 #! /bin/bash
 gnatmake -aI/Sources $1 -o exe
-echo "TECHIO> terminal -i ./exe"
-
-
-result=0
-RUNNING=0
-TERMINATED=0
-
-
-while [ $TERMINATED -ne 1 ];do
-	result=`ps aux | grep -i "myscript.sh" | grep -v "grep" | wc -l`
-	if [ $result -eq 2 ]
-		then RUNNING=1
-	else
-		if [ $result -eq 1 ]
-			then 
-				if [ $RUNNING -eq 1 ]
-					then TERMINATED=1
-				fi
-		fi
-	fi
-	sleep 0.1
-done
+echo "TECHIO> terminal"
+sleep 10
+./exe
