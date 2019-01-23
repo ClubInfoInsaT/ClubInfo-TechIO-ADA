5ea42a03ac5c3f3e7dad3c3176b657f6f1a73a76
Walleza1 <24304667+Walleza1@users.noreply.github.com>
2019-01-23 09:24:18 +0100

    Testing boucle

--- projets/Bonus/start.sh
+++ projets/Bonus/start.sh
@@ -1,4 +1,16 @@
 #! /bin/bash
-gnatmake -aI/Sources $1 -o exe
-echo "TECHIO> terminal -i ./exe"
-sleep 10
+gnatmake -aI/Sources $1 -o insa-ada-runner
+echo "TECHIO> terminal -i ./insa-ada-runner"
+running=0
+terminated=0
+while [ $terminated -ne 1 ];do
+	res=`ps -ef |grep "insa-ada-runner"|wc -l`
+	if [ $res -gt 1 ]
+		then running=1
+	else
+		if [ $running -eq 1 ]
+			then terminated=1
+		fi	
+	fi
+	sleep 1
+done
